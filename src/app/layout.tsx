779fc91b0248eeaa4c2fee582e3d385098b2dc1d
thisisaarush <tanwaraarush007@gmail.com>
2023-12-06 02:09:41 +0530

    updated with cssnano and font errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
+import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme/theme-provider"
 import TRPCProvider from "./_trpc/Provider"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans"
-})
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Full-Stack Custom NextJS App",
@@ -20,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>
           <TRPCProvider>
             <ThemeProvider
               attribute="class"
